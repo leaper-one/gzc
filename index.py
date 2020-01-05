9a30781dca2444d021b915e669862dd3386cc5d4
tymon42 <tymon42@163.com>
2020-01-05 21:56:02 +0800

    删除不成熟的restful api， 优化用户付款流程， 可以接受名片， 尝试发起对话，但不能成功生成UniqueConversationId

--- index.py
+++ index.py
@@ -25,6 +25,25 @@ api = Api(app)
 # app.config.from_object(__name__)
 mixin_api = MIXIN_API(mixin_config)
 
+
+@app.route('/')
+def index():
+    scope = 'PROFILE:READ+ASSETS:READ'
+    get_auth_code_url = 'https://mixin.one/oauth/authorize?client_id=' + mixin_config.client_id + '&scope='+ scope + '&response_type=code'
+    return redirect(get_auth_code_url)
+
+@app.route('/auth')
+def auth():
+    auth_token = fm.get_auth_token()
+    data = mixin_api.getMyProfile(auth_token)
+    return data
+
+
+@app.route('/happy')
+def happy():
+    return 'hello'
+
+
 '''
 关键账号
 '''
@@ -65,12 +84,6 @@ def generateMixinAPI(private_key,pin_token,session_id,user_id,pin,client_secret)
 # mixinApiNewUserInstance = generateMixinAPI(private_key, pin_token, session_id, userid, pin,"")
 
 
-class SigApi(Resource):
-    def creat(self, ws, userId, conversationId, dataindata, issig=True):
-        #添加到数据库 签名队列, 公证内容
-        # MIXIN_WS_API.sendUserAppButton(ws, conversationId, )
-        return {"issig"}
-
 
 try:
     import thread
@@ -105,12 +118,14 @@ def on_message(ws, message):
         typeindata = data["type"]
         categoryindata = data["category"]
         userId = data["user_id"]
+        # print(userId)
         conversationId = data["conversation_id"]
         dataindata = data["data"]
-
         realData = base64.b64decode(dataindata)
-
+        # print(realData.decode('utf-8'))
         MIXIN_WS_API.replayMessage(ws, msgid)
+        print(categoryindata)
+        print(typeindata)
 
         if 'error' in rdata_obj:
             return
@@ -119,58 +134,62 @@ def on_message(ws, message):
             realData = realData.decode('utf-8')
             print("_____-------______")
             print("dataindata",realData)
+            print("_____-------______")
+
             # print(type(realData))
 
             if dataindata:
-                MIXIN_WS_API.sendUserText(ws, conversationId, userId, "按下列按钮以进行操作")
-                MIXIN_WS_API.sendUserAppButton(ws, conversationId, userId, 'https://172.20.10.2/auth?code=8045c3b7048bd4e1670716ce4503715923613f75ad836b4e3a6ca1c0710ae779&state=', 'test')
+                step1isread = False
+                MIXIN_WS_API.sendUserText(ws, conversationId, userId, "按下列按钮以付款")
+                # MIXIN_WS_API.sendUserAppButton(ws, conversationId, userId, 'https://172.20.10.2/auth?code=8045c3b7048bd4e1670716ce4503715923613f75ad836b4e3a6ca1c0710ae779&state=', 'test')
 
                 # 储存到数据库
                 # ifsig = MIXIN_WS_API.sendUserAppButton(ws, conversationId, userId, "www.baidu.com", 'heppy')
-                a = MIXIN_WS_API.sendUserPayAppButton(ws, conversationId, userId, 'CNB', CNB, 1)
+
+                # a = MIXIN_WS_API.sendUserPayAppButton(ws, conversationId, userId, 'CNB', CNB, 1)
+                trace1 = fm.genTrace()
                 # MIXIN_WS_API.sendAppCard(ws, conversationId, userId, CNB, 1, '', 'lalaladifjsof', 'none', memo="123456")
-                trace = fm.genTrace()
-                pay_link = fm.genAPaylink(trace)
+                MIXIN_WS_API.sendUserPayAppButton(ws, conversationId, userId, '付款以邀请对方公证', CNB, 1, trace1)
+                # MIXIN_WS_API.sendUserText(ws, conversationId, userId, fm.genAPaylink(trace=trace1))
 
-                MIXIN_WS_API.sendUserText(ws, conversationId, userId, pay_link)
+                for i in range(0,20):
+                    print(mixin_api.verifyPayment(CNB, mixin_config.client_id, "1", trace1).get("data").get("status"))
+                    if mixin_api.verifyPayment(CNB, mixin_config.client_id, "1", trace1).get("data").get("status") == "pending":
+                        continue
+                    time.sleep(1)
+                    MIXIN_WS_API.sendUserText(ws, conversationId, userId, '请发送一个名片')
+                    step1isread = True
+                    time.sleep(1)
+                    if mixin_api.verifyPayment(CNB, mixin_config.client_id, "1", trace1).get("data").get("status") == "paid":
+                        break
+                    time.sleep(1)
 
-                time.sleep(1) #等待5分钟，后期应该成轮询
+        if categoryindata == "PLAIN_CONTACT":
+            print('111111111111')
+            MIXIN_WS_API.sendUserText(ws, conversationId, userId, '一个名片')
+            signer2 = eval(realData.decode('utf-8'))['user_id']
+            trace2 = fm.genTrace()
+            # print(trace2)
+            # new_conv_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, mixin_config.client_id))
+            '''
+            跪求大神来，解决一下：
+            https://docs.python.org/3.5/library/uuid.html
+            https://developers.mixin.one/api/beta-mixin-message/create-conversation/
+            '''
 
-                signer2paylink = mixin_api.verifyPayment(CNB, mixin_config.client_id, "1", trace)
-                # print(signer2paylink.get("data").get('status'))
+            a, b = mixin_config.client_id, signer2
+            if a >b:
+                new_conv_id = str(uuid.uuid3(uuid.NAMESPACE_X500, b+a))
+            else:
+                new_conv_id = str(uuid.uuid3(uuid.NAMESPACE_X500, a+b))
+            print(new_conv_id)
+            r= mixin_api.createConv('CONTACT', new_conv_id, 'ADD', '', signer2, '')
+            print(r)
 
-                if True or signer2paylink.get("data").get("status") == "paid": # 需要更改判断条件
-                    # 获取signer2 的 mixinid
-                    signer2 = ''
-                    # 更改 数据库 user表单 isalreadysign 字段 为 True
-                    pub_sign = fm.genAPressSignOfContra(userId,signer2,realData)
-                    print(pub_sign)
-
-
-                else:
-                    MIXIN_WS_API.sendUserText(ws, conversationId, userId, pay_link)
+            print('2222222')
 
 
-@app.route('/')
-def index():
-    return 'hello'
-
-# 一个 请求 回复的实例 https://mixin.one/auth?code=8045c3b7048bd4e1670716ce4503715923613f75ad836b4e3a6ca1c0710ae779&state=
-'''
-! 未完成
-https://mixin-network.gitbook.io/mixin-network-cn/messenger-ying-yong-kai-fa/huo-qu-messenger-yong-hu-de-xin-xi
-#获取messenger用户的信息
-
- 一个 请求 回复的实例 https://mixin.one/auth?code=8045c3b7048bd4e1670716ce4503715923613f75ad836b4e3a6ca1c0710ae779&state=
-以下方法 用于获取实例中的code
-如有需要改动机器人跳转 OAuth_redirect url ，请务必联系 tymon
-'''
-@app.route('/auth', methods=['GET'])
-def get_code():
-    if request.method == 'GET':
-        code = request.args['code']
-        print(code)
-    return code
+            # MIXIN_WS_API.sendUserText(ws, conversationId, userId, fm.genAPaylink(trace=trace))
 
 
 if __name__ == "__main__":
@@ -179,3 +198,4 @@ if __name__ == "__main__":
     while True:
         mixin_ws = MIXIN_WS_API(on_message=on_message)
         mixin_ws.run()
+        # app.run(debug=True, host='0.0.0.0')
